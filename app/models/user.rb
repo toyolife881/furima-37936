9e133177b9c2f1f647f0bf3e7ffcd79e8588e651
toyolife881 <toyolife.island881@gmail.com>
2022-07-14 22:30:31 +0900

    正規表現の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_day, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: {with: PASSWORD_REGEX}
+
+  ZENKAKU_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  validates :last_name, presence: true, format: {with:ZENKAKU_REGEX}
+  validates :first_name, presence: true, format: {with:ZENKAKU_REGEX}
+
+  KANA_REGEX = /\A[ァ-ヶー]+\z/.freeze
+  validates :last_name_kana, presence: true, format: {with:KANA_REGEX}
+  validates :first_name_kana, presence: true, format: {with:KANA_REGEX}
+
 end
