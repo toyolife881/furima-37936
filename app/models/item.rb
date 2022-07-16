bbb0d7c10bb38fc52adb1cf0ac7bc58d5d17b211
toyolife881 <toyolife.island881@gmail.com>
2022-07-17 06:32:05 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :item_name, presence: true
+  validates :item_explanation, presence: true
+  validates :item_category_id, presence: true
+  validates :item_status_id, presence: true
+  validates :burden_of_charge_id, presence: true
+  validates :delivery_prefecture_id, presence: true
+  validates :delivery_days_id, presence: true
+  validates :price, presence: true
+
 end
