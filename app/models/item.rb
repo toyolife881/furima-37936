dcb29b109359e90b65a5b9bc539c24d17d4eae07
toyolife881 <toyolife.island881@gmail.com>
2022-07-17 07:13:29 +0900

    プルダウン項目のアソシエーション、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,18 @@ class Item < ApplicationRecord
   validates :delivery_days_id, presence: true
   validates :price, presence: true
 
+
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_status
+  belongs_to :burden_of_charge
+  belongs_to :delivery_prefecture
+  belongs_to :delivery_days
+
+  validates :item_category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :item_status_id, numericality: { other_than: 1, message: "can't be blank" }  
+  validates :burden_of_charge_id, numericality: { other_than: 1, message: "can't be blank" }  
+  validates :delivery_prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_days_id, numericality: { other_than: 1, message: "can't be blank" } 
 end
