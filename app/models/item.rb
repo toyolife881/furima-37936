e6a25643ee8e31efb94e664257d03254ab0b38e3
toyolife881 <toyolife.island881@gmail.com>
2022-07-19 15:59:29 +0900

    モデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,8 @@ class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :item_explanation, presence: true
 
-  HANKAKU_REGEX = /\A[0-9]+\z/.freeze
   validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: HANKAKU_REGEX }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
