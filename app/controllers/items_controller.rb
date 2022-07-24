b351ea87b9d0ae8306c21c5b8a4718239bbdcfff
toyolife881 <toyolife.island881@gmail.com>
2022-07-24 11:12:48 +0900

    メソッド(set_item)の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :basic_auth
   before_action :authenticate_user!, only:[:new, :edit]
+  before_action :set_item, only:[:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,18 +21,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id 
      redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.save
      redirect_to item_path(@item)
@@ -45,6 +44,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :item_explanation, :item_category_id, :item_status_id, :burden_of_charge_id,
                                  :delivery_prefecture_id, :delivery_days_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
 
