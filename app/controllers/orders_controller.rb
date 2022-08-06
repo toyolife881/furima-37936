c7676dc1c6829b87e9169ef8c8f9b90d593b630b
toyolife881 <toyolife.island881@gmail.com>
2022-08-07 04:24:12 +0900

    環境変数の設定(開発環境)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -21,7 +21,8 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_a25027a6ca4a30b4511d47d5"
+    # Payjp.api_key = "sk_test_a25027a6ca4a30b4511d47d5"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
         amount: @item.price,  # 商品の値段
         card: order_params[:token],    # カードトークン
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_e4e33f519fe8fab7534c8ffb"); // PAY.JPテスト公開鍵
+  // Payjp.setPublicKey("pk_test_e4e33f519fe8fab7534c8ffb"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
